dfb27749585a8a1351228fbef7a5fbbff98c1acf
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2023-11-20 16:04:53 +0530

    Initial commit

---
+++ src/test/java/mypackage/MyClass.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class MyClass {
+
+	public void Test() {
+		System.out.println("Welcome");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
